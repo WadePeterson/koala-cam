346fce2832473152274ed96796ab55fa67d15d58
Wade Peterson and Garston Tremblay <wpeterson@rallydev.com>
2015-03-31 10:11:28 -0600

    Trimming highlights based on actual duration

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var ffmpeg = require('fluent-ffmpeg');
+var fs = require('fs');
 var app = express();
+
+app.listen(3000, function () {
+    deleteTempFiles();
+    startRecording();
+    setInterval(saveHighlight, 10000);
+});
 
 app.get('/', function(req,res) {
     res.sendfile('index.html');
@@ -14,10 +21,20 @@ app.get('/record', function (req, res) {
 
 });
 
-var highlightIndex = 0;
+var highlightDuration = 2;
 var command;
+var waitingToRecord = false;
+var isRecording = false;
 
-var startRecording = function() {
+function startRecording() {
+    if (isRecording || waitingToRecord) {
+        return;
+    }
+
+    waitingToRecord = true;
+
+    var tempRecordingName = 'temp/recording-' + currentTimeMillis() + '.mp4';
+
     command = ffmpeg('default')
         .inputOptions([
             '-video_device_index 0',
@@ -30,17 +47,62 @@ var startRecording = function() {
         .audioCodec('libmp3lame')
         .videoCodec('libx264')
         .duration('30:00')
-        .save('videos/highlights-' + highlightIndex++ + '.mp4');
-};
+        .on('start', function(commandLine) {
+            isRecording = true;
+            waitingToRecord = false;
+            console.log('Transcoding started with command: ' + commandLine);
+        })
+        .on('end', function() {
+            // Probably don't want this to record a highlight....
+            onStopRecording(tempRecordingName);
+            console.log('Transcoding ended. What does that mean?!?!?');
+        })
+        .on('error', function (err) {
+            onStopRecording(tempRecordingName);
+            console.log('Error or intentional stoppage while transcoding video: ', err.message)
+        })
+        .save(tempRecordingName);
+}
 
-app.listen(3000, function () {
+function onStopRecording(tempRecordingName) {
+    isRecording = false;
+    waitingToRecord = false;
+
+    ffmpeg(tempRecordingName).ffprobe(function(err, metadata) {
+        ffmpeg(tempRecordingName)
+            .seekInput(Math.max(metadata.streams[0].duration - highlightDuration, 0))
+            .on('error', function (err) {
+                console.log('Error saving highlight video: ', err.message);
+                deleteTempFile(tempRecordingName);
+            })
+            .on('end', function () {
+                deleteTempFile(tempRecordingName);
+            })
+            .save('highlights/highlight-' + currentTimeMillis() + '.mp4')
+    });
+}
+
+function saveHighlight() {
+    if (isRecording || waitingToRecord) {
+        command.kill('SIGINT');
+    }
+
     startRecording();
+}
 
-    setInterval(function(){
-        command
-            .on('error', function(){})
-            .kill('SIGINT');
+function deleteTempFile(fileName) {
+    try {
+        // TODO: Fix this
+//        fs.unlink(fileName);
+    } catch(err) {
+        console.log('Error deleting temp file: ' + err.message);
+    }
+}
 
-        startRecording();
-    }, 10000);
-});
+function deleteTempFiles() {
+    // TODO: Loop over temp files. Async?
+}
+
+function currentTimeMillis() {
+    return new Date().getTime();
+}
